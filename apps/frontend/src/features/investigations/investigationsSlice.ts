dd40f106215b249763a6a3d61ebeddadcc320682
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-03-05 17:19:21 -0800

    Refactored how investigations are managed in our redux state, notably how they are stored, fetched, and filtered. Selectors and a new reducer were added to handle filtering.

--- apps/frontend/src/features/investigations/investigationsSlice.ts
+++ apps/frontend/src/features/investigations/investigationsSlice.ts
@@ -1,52 +1,38 @@
 import axios from 'axios';
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, createSelector, PayloadAction } from "@reduxjs/toolkit";
 
 import { Investigation } from "src/types/investigation.d";
 import { PDS4_INFO_MODEL } from "src/types/pds4-info-model";
 
 enum INVESTIGATION_ACTIONS {
   GET_INVESTIGATION = "investigations/getInvestigation",
-  GET_INVESTIGATIONS = "investigations/getInvestigations"
+  GET_INVESTIGATIONS = "investigations/getInvestigations",
+  SET_INVESTIGATIONS_FILTER = "investigations/setFilter",
 }
 
-interface InvestigationsStateInterface {
-  investigationItems: Investigation[]
-  status: 'idle' | 'pending' | 'succeeded' | 'failed'
-  error: string | null | undefined
-}
-
-const initialState = {
-  investigationItems: [],
-  status: 'idle',
-  error: null
-} as InvestigationsStateInterface
-
-const parseLID = (lid:string) => {
-  
-  const re = /urn:(.+):(.+):context:(.+):(.*)\.(.*)$/i;
-  const matches = lid.match(re);
-  
-  if( matches != undefined && matches?.length > 0 ) {
-    
-    const agency = matches[1];
-    const archive = matches[2];
-    const category = matches[3];
-    const investigationTypeParts = matches[4].split("."); 
-    const investigationType = investigationTypeParts[0];
-    const investigationSubType = investigationTypeParts.length > 0 ? investigationTypeParts[1] : undefined;
-    const identifier = matches[5];
-    
-    return { agency, archive, category, investigationType, investigationSubType, identifier }
-    
-  } else {
-    console.log("Unable to parse LID:", lid)
-  }
-  
-  return null
-  
+type InvestigationItems = { 
+  [index:string]: { 
+    [index:string]: Investigation 
+  } 
 };
 
-// Get all the investigations from the PDS OpenSearch API
+type InvestigationsState = {
+  error: string | null | undefined
+  items: InvestigationItems
+  searchFilter:string
+  status: 'idle' | 'pending' | 'succeeded' | 'failed'
+};
+
+const initialState:InvestigationsState = {
+  error: null,
+  items: {},
+  searchFilter: "",
+  status: 'idle',
+};
+
+/**
+ * Get all the investigations from the PDS OpenSearch API
+ */ 
 export const getInvestigations = createAsyncThunk(
   INVESTIGATION_ACTIONS.GET_INVESTIGATIONS,
   async (_:void, thunkAPI) => {
@@ -63,7 +49,7 @@ export const getInvestigations = createAsyncThunk(
     // Set up query
     const query = {
       "from": 0,
-      "size": 500,
+      "size": 9999, // we must provide a size otherwise we only receive a limited number of results
       "query": {
         "wildcard": {
           [PDS4_INFO_MODEL.INVESTIGATION.DESCRIPTION]: {
@@ -74,6 +60,7 @@ export const getInvestigations = createAsyncThunk(
       "_source": {
         "includes": [
           PDS4_INFO_MODEL.LID,
+          PDS4_INFO_MODEL.LIDVID,
           PDS4_INFO_MODEL.VID,
           PDS4_INFO_MODEL.ALIAS.ALTERNATE_ID,
           PDS4_INFO_MODEL.ALIAS.ALTERNATE_TITLE,
@@ -98,30 +85,14 @@ export const getInvestigations = createAsyncThunk(
   }
 );
 
-const getInvestigationIndex = (arr_to_check:Investigation[], lid:string, version:string):number | undefined => { 
-  
-
-  const arrayIndex:number = arr_to_check.findIndex( (item) => 
-    item.lid == lid
-  );
-
-  if( arrayIndex !== -1 ) {
-
-    if( arr_to_check[arrayIndex][PDS4_INFO_MODEL.VID] < version ) {
-      return arrayIndex;
-    } else {
-      return undefined
-    }
-  }
-  
-  return -1;
-  
-};
-
 const investigationsSlice = createSlice({
   name: "investigations",
   initialState,
-  reducers: {},
+  reducers: {
+    setSearchFilter: (state, action:PayloadAction<string>) => {
+      state.searchFilter = action.payload;
+    },
+  },
   extraReducers: (builder) => {
     
     builder.addCase(getInvestigations.pending, (state, action) => {
@@ -129,55 +100,45 @@ const investigationsSlice = createSlice({
       state.status = "pending";
     });
     
-    builder.addCase(getInvestigations.fulfilled, (state, action) => {
+    builder .addCase(getInvestigations.fulfilled, (state, action) => {
       // When data is fetched successfully
       state.status = "succeeded";
       
       // Store the fetched data into the state after parsing
-      const investigationData = action.payload.hits.hits
-      
-      let compiledInvestigations:Investigation[] = [];
-      for( var i = 0; i < investigationData.length; i++ ) {
-        
-        const lid = investigationData[i]._source[PDS4_INFO_MODEL.LID];
-        const investigationMetadata = parseLID(lid);
-        
-        if( investigationMetadata != null ) {
-          
-          const version = investigationData[i]._source[PDS4_INFO_MODEL.VID];
-          
-          const investigationIndex:number | undefined = getInvestigationIndex(compiledInvestigations, lid, version);
-          let investigation:Investigation = <Investigation>{
-            [PDS4_INFO_MODEL.ALIAS.ALTERNATE_TITLE]: PDS4_INFO_MODEL.ALIAS.ALTERNATE_TITLE in investigationData[i]._source ? investigationData[i]._source[PDS4_INFO_MODEL.ALIAS.ALTERNATE_TITLE][0] : "",
-            [PDS4_INFO_MODEL.INVESTIGATION.DESCRIPTION]: investigationData[i]._source[PDS4_INFO_MODEL.INVESTIGATION.DESCRIPTION][0],
-            [PDS4_INFO_MODEL.LID]: lid,
-            [PDS4_INFO_MODEL.INVESTIGATION.START_DATE]: investigationData[i]._source[PDS4_INFO_MODEL.INVESTIGATION.START_DATE][0],
-            [PDS4_INFO_MODEL.INVESTIGATION.STOP_DATE]: investigationData[i]._source[PDS4_INFO_MODEL.INVESTIGATION.STOP_DATE][0],
-            [PDS4_INFO_MODEL.IDENTIFICATION_AREA.TITLE]: investigationData[i]._source[PDS4_INFO_MODEL.IDENTIFICATION_AREA.TITLE][0],
-            [PDS4_INFO_MODEL.IDENTIFICATION_AREA.VERSION_ID]: investigationData[i]._source[PDS4_INFO_MODEL.IDENTIFICATION_AREA.VERSION_ID][0],
-            [PDS4_INFO_MODEL.INVESTIGATION.TYPE]: investigationData[i]._source[PDS4_INFO_MODEL.INVESTIGATION.TYPE][0],
-            [PDS4_INFO_MODEL.VID]: investigationData[i]._source[PDS4_INFO_MODEL.VID],
-          }
+      const data = action.payload.hits.hits;
 
-          if( investigationIndex === -1 ) {
-            compiledInvestigations.push(investigation);
-          } else if( investigationIndex !== undefined ) {
-            compiledInvestigations[investigationIndex] = investigation;
-          }
+      let compiledInvestigations:InvestigationItems = {};
+      data.forEach( (element:{_source:object}) => {
 
+        const source:Investigation = <Investigation>element["_source"];
+
+        const lid = source[PDS4_INFO_MODEL.LID];
+        const vid = source[PDS4_INFO_MODEL.VID];
+
+        let investigationItem:Investigation = <Investigation>{};
+        investigationItem[PDS4_INFO_MODEL.LID] = source[PDS4_INFO_MODEL.LID];
+        investigationItem[PDS4_INFO_MODEL.LIDVID] = source[PDS4_INFO_MODEL.LIDVID];
+        investigationItem[PDS4_INFO_MODEL.VID] = source[PDS4_INFO_MODEL.VID];
+        investigationItem[PDS4_INFO_MODEL.ALIAS.ALTERNATE_ID] = source[PDS4_INFO_MODEL.ALIAS.ALTERNATE_ID];
+        investigationItem[PDS4_INFO_MODEL.ALIAS.ALTERNATE_TITLE] = source[PDS4_INFO_MODEL.ALIAS.ALTERNATE_TITLE];
+        investigationItem[PDS4_INFO_MODEL.IDENTIFICATION_AREA.TITLE] = source[PDS4_INFO_MODEL.IDENTIFICATION_AREA.TITLE][0];
+        investigationItem[PDS4_INFO_MODEL.IDENTIFICATION_AREA.VERSION_ID] = source[PDS4_INFO_MODEL.IDENTIFICATION_AREA.VERSION_ID][0];
+        investigationItem[PDS4_INFO_MODEL.INVESTIGATION.DESCRIPTION] = source[PDS4_INFO_MODEL.INVESTIGATION.DESCRIPTION][0];
+        investigationItem[PDS4_INFO_MODEL.INVESTIGATION.NAME] = source[PDS4_INFO_MODEL.INVESTIGATION.NAME] ? source[PDS4_INFO_MODEL.INVESTIGATION.NAME][0] : "";
+        investigationItem[PDS4_INFO_MODEL.INVESTIGATION.START_DATE] = source[PDS4_INFO_MODEL.INVESTIGATION.START_DATE][0];
+        investigationItem[PDS4_INFO_MODEL.INVESTIGATION.STOP_DATE] = source[PDS4_INFO_MODEL.INVESTIGATION.STOP_DATE][0];
+        investigationItem[PDS4_INFO_MODEL.INVESTIGATION.TYPE] = source[PDS4_INFO_MODEL.INVESTIGATION.TYPE][0];
+
+        if( compiledInvestigations[lid] === undefined ) {
+          compiledInvestigations[lid] = {};
         }
-        
-      }
-      state.investigationItems = compiledInvestigations.sort( (a,b) => {
-        if( a["pds:Identification_Area/pds:title"].toLowerCase() < b["pds:Identification_Area/pds:title"].toLowerCase() ) {
-          return -1
-        }
-        if( a["pds:Identification_Area/pds:title"].toLowerCase() > b["pds:Identification_Area/pds:title"].toLowerCase() ) {
-          return 1
-        }
-        return 0
+
+        compiledInvestigations[lid][vid] = <Investigation>investigationItem;
+
       });
       
+      state.items = compiledInvestigations;
+
     });
     
     builder.addCase(getInvestigations.rejected, (state, action) => {
@@ -191,8 +152,50 @@ const investigationsSlice = createSlice({
   }
 });
 
-export const selectFilteredInvestigations = (state:InvestigationsStateInterface, filter:string) => {
-  return state.investigationItems.filter( (item) => item[PDS4_INFO_MODEL.IDENTIFICATION_AREA.TITLE].startsWith(filter))
-}
+/**
+ * A redux selector to efficiently retrive a list of the latest investigation.
+ * @param {InvestigationsState} state The invesigations redux state of type InvestigationsState
+ * @returns {Investigation[]} An array containing the list of the latest versions of all investigations
+ */
+const selectLatestVersionInvestigations = (state:InvestigationsState):Investigation[] => {
 
+  let investigations:Investigation[] = [];
+
+  Object.keys(state.items).forEach( (lid) => {
+    const latestVersion = Object.keys(state.items[lid]).sort().reverse()[0];
+    investigations.push( state.items[lid][latestVersion] );
+  });
+
+  return investigations;
+
+};
+
+/**
+ * The search filter that should be applied to the list of investigations.
+ * @param {InvestigationsState} state The invesigations redux state of type InvestigationsState
+ * @returns {string} The search filter currently being applied
+ */
+const selectFilter = (state:InvestigationsState):string => {
+  return state.searchFilter;
+};
+
+/**
+ * A memoized redux selector that efficiently returns the latest, and filtered list of investigations.
+ * @returns {Investigation[]} An filtered, and latest list of investigations
+ */
+export const selectFilteredInvestigations = createSelector([selectLatestVersionInvestigations, selectFilter], (latestInvestigations:Investigation[], searchFilter:string) => {
+
+  if( searchFilter === "" ) {
+    return latestInvestigations;
+  }
+
+  return latestInvestigations.filter(
+    (item) => { 
+      return item[PDS4_INFO_MODEL.IDENTIFICATION_AREA.TITLE].toLowerCase().includes(searchFilter)
+    }
+  );
+
+});
+
+export const { setSearchFilter } = investigationsSlice.actions;
 export default investigationsSlice.reducer;
