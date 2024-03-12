f41669bbd791f2256125e8916c26821fe2778b56
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-03-12 14:19:53 -0700

    Removed extraneous testing code from investigations slice.

--- apps/frontend/src/state/slices/investigationsSlice.ts
+++ apps/frontend/src/state/slices/investigationsSlice.ts
@@ -89,78 +89,6 @@ export const getInvestigations = createAsyncThunk(
   }
 );
 
-export const getEverything = createAsyncThunk(
-  'pds/data/get_everything',
-  async(_:void, thunkAPI) => {
-
-    const username = import.meta.env.VITE_OPENSEARCH_USERNAME;
-    const password = import.meta.env.VITE_OPENSEARCH_PASSWORD;
-    const url = import.meta.env.VITE_OPENSEARCH_DOMAIN + "_msearch";
-    
-    const headers = {
-      "Content-Type": "application/x-ndjson",
-      "Authorization": "Basic " + btoa(username + ":" + password)
-    }
-
-    // Set up query
-    const query = `
-      { "index": [ "registry", "atm-prod-ccs:registry", "img-prod-ccs:registry" ] }
-      { "query": { "wildcard": { "lid": { "value": "urn:*:investigation:*" } } }, "_source": { "includes": [ "${PDS4_INFO_MODEL.LID}", "lidvid", "pds:Identification_Area/pds:name", "pds:Identification_Area/pds:title", "pds:Investigation/pds:name", "pds:Investigation/pds:description", "pds:Investigation/pds:start_date", "pds:Investigation/pds:stop_date", "pds:Investigation/pds:type", "pds:Alias/pds:alternate_id", "pds:Alias/pds:alternate_title" ] }, "from": 0, "size": 9999 }
-      { "index": [ "registry", "atm-prod-ccs:registry", "img-prod-ccs:registry" ] }
-      { "query": { "wildcard": { "lid": { "value": "urn:*:target:*" } } }, "_source": { "includes": [ "${PDS4_INFO_MODEL.LID}" ] }, "from": 0, "size": 9999 }
-      { "index": [ "registry", "atm-prod-ccs:registry", "img-prod-ccs:registry" ] }
-      { "query": { "wildcard": { "lid": { "value": "urn:*:instrument_host:*" } } }, "_source": { "includes": [ "${PDS4_INFO_MODEL.LID}" ] }, "from": 0, "size": 9999 }
-      { "index": [ "registry", "atm-prod-ccs:registry", "img-prod-ccs:registry" ] }
-      { "query": { "wildcard": { "lid": { "value": "urn:*:instrument:*" } } }, "_source": { "includes": [ "${PDS4_INFO_MODEL.LID}" ] }, "from": 0, "size": 9999 }
-    ` + "\n";
-    
-    try {
-      const response = await axios.post(url, query, {headers: headers})   // missing newline error
-      return response.data
-    } catch (err:any) {
-      return thunkAPI.rejectWithValue({ error: err.message });
-    }
-
-  }
-)
-
-const updateInvestigationsState = (state:InvestigationsState, data) => {
-
-  let compiledInvestigations:InvestigationItems = {};
-  data.forEach( (element:{_source:object}) => {
-
-    const source:Investigation = <Investigation>element["_source"];
-
-    const lid = source[PDS4_INFO_MODEL.LID];
-    const vid = source[PDS4_INFO_MODEL.VID];
-
-    let investigationItem:Investigation = <Investigation>{};
-    investigationItem[PDS4_INFO_MODEL.LID] = source[PDS4_INFO_MODEL.LID];
-    investigationItem[PDS4_INFO_MODEL.LIDVID] = source[PDS4_INFO_MODEL.LIDVID];
-    investigationItem[PDS4_INFO_MODEL.VID] = source[PDS4_INFO_MODEL.VID];
-    investigationItem[PDS4_INFO_MODEL.ALIAS.ALTERNATE_ID] = source[PDS4_INFO_MODEL.ALIAS.ALTERNATE_ID];
-    investigationItem[PDS4_INFO_MODEL.ALIAS.ALTERNATE_TITLE] = source[PDS4_INFO_MODEL.ALIAS.ALTERNATE_TITLE];
-    investigationItem[PDS4_INFO_MODEL.IDENTIFICATION_AREA.TITLE] = source[PDS4_INFO_MODEL.IDENTIFICATION_AREA.TITLE][0];
-    investigationItem[PDS4_INFO_MODEL.IDENTIFICATION_AREA.VERSION_ID] = source[PDS4_INFO_MODEL.IDENTIFICATION_AREA.VERSION_ID][0];
-    investigationItem[PDS4_INFO_MODEL.INVESTIGATION.DESCRIPTION] = source[PDS4_INFO_MODEL.INVESTIGATION.DESCRIPTION][0];
-    investigationItem[PDS4_INFO_MODEL.INVESTIGATION.NAME] = source[PDS4_INFO_MODEL.INVESTIGATION.NAME] ? source[PDS4_INFO_MODEL.INVESTIGATION.NAME][0] : "";
-    investigationItem[PDS4_INFO_MODEL.INVESTIGATION.START_DATE] = source[PDS4_INFO_MODEL.INVESTIGATION.START_DATE][0];
-    investigationItem[PDS4_INFO_MODEL.INVESTIGATION.STOP_DATE] = source[PDS4_INFO_MODEL.INVESTIGATION.STOP_DATE][0];
-    investigationItem[PDS4_INFO_MODEL.INVESTIGATION.TYPE] = source[PDS4_INFO_MODEL.INVESTIGATION.TYPE][0];
-
-    if( compiledInvestigations[lid] === undefined ) {
-      compiledInvestigations[lid] = {};
-    }
-
-    compiledInvestigations[lid][vid] = <Investigation>investigationItem;
-
-  });
-  
-  state.items = compiledInvestigations;
-
-}
-
-
 const investigationsSlice = createSlice({
   name: "investigations",
   initialState,
@@ -234,46 +162,16 @@ const investigationsSlice = createSlice({
       state.error = action.error.message;
     });
 
-    builder.addCase(getEverything.pending, (state, _action) => {
-      // When data is being fetched
-      state.status = "pending";
-    });
-    
-    builder.addCase(getEverything.fulfilled, (state, action) => {
-      // When data is fetched successfully
-      state.status = "succeeded";
-      console.log("SUCCESS!")
-      
-      updateInvestigationsState(state, action.payload.responses[0].hits.hits);
-
-      // Store the fetched data into the state after parsing
-      /*console.log("Investigations: ", action.payload.responses[0]);
-      console.log("Targets: ", action.payload.responses[1]);
-      console.log("Instrument Hosts: ", action.payload.responses[2]);
-      console.log("Instruments: ", action.payload.responses[3]);*/
-
-      const data = action.payload.hits.hits;
-
-    });
-    
-    builder.addCase(getEverything.rejected, (state, action) => {
-      // When data is fetched unsuccessfully
-      state.status = "failed";
-      
-      // Update the error message for proper error handling
-      state.error = action.error.message;
-    });
-    
   }
 });
 
 /**
- * A redux selector to efficiently retrive a list of the latest investigation.
+ * A redux selector to efficiently retrieve a list of the latest investigation.
  * @param {InvestigationsState} state The invesigations redux state of type InvestigationsState
  * @returns {Investigation[]} An array containing the list of the latest versions of all investigations
  */
-const selectLatestVersionInvestigations = (state:InvestigationsState): InvestigationItems => {
-  return state.items;
+const selectLatestVersionInvestigations = (state:RootState): InvestigationItems => {
+  return state.investigations.items;
 };
 
 /**
