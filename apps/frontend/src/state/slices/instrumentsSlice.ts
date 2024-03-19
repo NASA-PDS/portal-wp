925937866253b29fa9ca40775599f357c7558044
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-03-19 11:55:52 -0700

    Moved instrument selectors from instruments slice to its own selector file.

--- apps/frontend/src/state/slices/instrumentsSlice.ts
+++ apps/frontend/src/state/slices/instrumentsSlice.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { createSlice, createAsyncThunk, createSelector } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 import { Instrument } from "src/types/instrument.d";
 import { PDS4_INFO_MODEL } from "src/types/pds4-info-model";
@@ -126,42 +126,6 @@ const instrumentsSlice = createSlice({
     });
     
   }
-});
-
-/**
- * A redux selector to efficiently retrieve a list of the latest instruments.
- * @param {InstrumentsState} state The instruments redux state of type InstrumentsState
- * @returns {Instrument[]} An array containing the list of the latest versions of all instruments
- */
-export const selectLatestVersionInstruments = (state:InstrumentsState): InstrumentItems => {
-  return state.items;
-};
-
-/**
- * A memoized redux selector that efficiently returns the latest, and filtered list of instruments.
- * @returns {Instrument[]} An filtered, and latest list of instruments
- */
-export const selectFilteredInstruments = createSelector([selectLatestVersionInstruments], (instruments:InstrumentItems) => {
-
-  let latestInstruments:Instrument[] = [];
-  
-  // Find the latest version of each instrument and store it in an array
-  let latestVersion:string = "";
-  Object.keys(instruments).forEach( (lid) => {
-    latestVersion = Object.keys(instruments[lid]).sort().reverse()[0];
-    latestInstruments.push( instruments[lid][latestVersion] );
-  });
-  
-  // Sort instruments alphabetically by title
-  latestInstruments.sort( (a:Instrument,b:Instrument) => {
-    if( a[PDS4_INFO_MODEL.TITLE].toLowerCase() < b[PDS4_INFO_MODEL.TITLE].toLowerCase() ) {
-      return -1
-    } else if( a[PDS4_INFO_MODEL.TITLE].toLowerCase() > b[PDS4_INFO_MODEL.TITLE].toLowerCase() ) {
-      return 1
-    }
-    return 0;
-  });
-
 });
 
 export default instrumentsSlice.reducer;
