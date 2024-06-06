ffe8dbb143f180d1c4c30a9b0c5be416dc959f60
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-06-06 10:36:25 -0700

    Updated Targets Slice to read from Search API.

--- apps/frontend/src/state/slices/targetsSlice.ts
+++ apps/frontend/src/state/slices/targetsSlice.ts
@@ -27,48 +27,41 @@ const initialState:TargetsState = {
 };
 
 /**
- * Get all the targets from the PDS OpenSearch API
+ * Get all the targets from the PDS Search API
  */ 
 export const getTargets = createAsyncThunk(
   TARGET_ACTIONS.GET_TARGETS,
   async (_:void, thunkAPI) => {
     
-    const username = import.meta.env.VITE_OPENSEARCH_USERNAME;
-    const password = import.meta.env.VITE_OPENSEARCH_PASSWORD;
-    const url = import.meta.env.VITE_OPENSEARCH_DOMAIN + import.meta.env.VITE_OPENSEARCH_INDEX_REGISTRY + "/_search";
-    
-    const headers = {
-      "Content-Type": "application/json",
-      "Authorization": "Basic " + btoa(username + ":" + password)
-    }
-    
-    // Set up query
-    const query = {
-      "from": 0,
-      "size": 9999, // we must provide a size otherwise we only receive a limited number of results
-      "query": {
-        "wildcard": {
-          [PDS4_INFO_MODEL.LID]: {
-            "value": "urn:*:target:*"
-          }
-        }
-      },
-      "_source": {
-        "includes": [
-          PDS4_INFO_MODEL.LID,
-          PDS4_INFO_MODEL.LIDVID,
-          PDS4_INFO_MODEL.TITLE,
-          PDS4_INFO_MODEL.VID,
-          PDS4_INFO_MODEL.TARGET.DESCRIPTION,
-          PDS4_INFO_MODEL.TARGET.NAME,
-          PDS4_INFO_MODEL.TARGET.TYPE
-        ]
+    let apiUrl = '/api/search/1/products?q=(product_class eq "Product_Context" and lid like "urn:nasa:pds:context:target:*")&limit=9999'
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
       }
     };
     
+    const fields = [
+      PDS4_INFO_MODEL.LID,
+      PDS4_INFO_MODEL.LIDVID,
+      PDS4_INFO_MODEL.TITLE,
+      PDS4_INFO_MODEL.VID,
+      PDS4_INFO_MODEL.TARGET.DESCRIPTION,
+      PDS4_INFO_MODEL.TARGET.NAME,
+      PDS4_INFO_MODEL.TARGET.TYPE
+    ];
+
+    // Add the specific fields that should be returned
+    apiUrl += "&fields=";
+    fields.forEach( (field, index) => {
+      apiUrl += field;
+      apiUrl += index < fields.length - 1 ? "," : "";
+    });
+    
     try {
-      const response = await axios.post(url, query, {headers: headers})
-      return response.data
+      
+      //const response = await fetch(apiUrl, config);
+      const response = await axios.get(apiUrl, config);
+      return response.data;
     } catch (err:unknown) {
       if( err instanceof Error ) {
         return thunkAPI.rejectWithValue({ error: err.message });
@@ -94,24 +87,26 @@ const targetsSlice = createSlice({
       state.status = "succeeded";
       
       // Store the fetched data into the state after parsing
-      const data = action.payload.hits.hits;
+      const data = action.payload.data;
+      console.log("Returned Targets Data:", data);
 
       const compiledItems:TargetItems = {};
-      data.forEach( (element:{_source:object}) => {
+      data.forEach( (element:{"summary":object, "properties":object}) => {
 
-        const source:Target = <Target>element["_source"];
+        const source:Target = <Target>element["properties"];
+        console.log("source:", source);
 
-        const lid = source[PDS4_INFO_MODEL.LID];
-        const vid = source[PDS4_INFO_MODEL.VID];
+        const lid = source[PDS4_INFO_MODEL.LID][0];
+        const vid = source[PDS4_INFO_MODEL.VID][0];
 
         const target:Target = <Target>{};
-        target[PDS4_INFO_MODEL.LID] = source[PDS4_INFO_MODEL.LID];
-        target[PDS4_INFO_MODEL.LIDVID] = source[PDS4_INFO_MODEL.LIDVID];
-        target[PDS4_INFO_MODEL.TITLE] = source[PDS4_INFO_MODEL.TITLE];
-        target[PDS4_INFO_MODEL.VID] = source[PDS4_INFO_MODEL.VID];
-        target[PDS4_INFO_MODEL.TARGET.DESCRIPTION] = source[PDS4_INFO_MODEL.TARGET.DESCRIPTION];
-        target[PDS4_INFO_MODEL.TARGET.NAME] = source[PDS4_INFO_MODEL.TARGET.NAME];
-        target[PDS4_INFO_MODEL.TARGET.TYPE] = source[PDS4_INFO_MODEL.TARGET.TYPE];
+        target[PDS4_INFO_MODEL.LID] = source[PDS4_INFO_MODEL.LID][0];
+        target[PDS4_INFO_MODEL.LIDVID] = source[PDS4_INFO_MODEL.LIDVID][0];
+        target[PDS4_INFO_MODEL.TITLE] = source[PDS4_INFO_MODEL.TITLE][0];
+        target[PDS4_INFO_MODEL.VID] = source[PDS4_INFO_MODEL.VID][0];
+        target[PDS4_INFO_MODEL.TARGET.DESCRIPTION] = source[PDS4_INFO_MODEL.TARGET.DESCRIPTION] ? source[PDS4_INFO_MODEL.TARGET.DESCRIPTION] : "";
+        target[PDS4_INFO_MODEL.TARGET.NAME] = source[PDS4_INFO_MODEL.TARGET.NAME] ? source[PDS4_INFO_MODEL.TARGET.NAME][0] : "";
+        target[PDS4_INFO_MODEL.TARGET.TYPE] = source[PDS4_INFO_MODEL.TARGET.TYPE] ? source[PDS4_INFO_MODEL.TARGET.TYPE][0] : "";
 
         if( compiledItems[lid] === undefined ) {
           compiledItems[lid] = {};
