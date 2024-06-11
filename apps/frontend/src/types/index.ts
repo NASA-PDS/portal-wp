39895a62668be84a1a58f81a51cf87f5db3cdbe9
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-06-11 13:26:58 -0700

    Added missing type definition.

---
+++ apps/frontend/src/types/index.ts
@@ -0,0 +1,6 @@
+import { Instrument } from "./instrument.d";
+import { InstrumentHost } from "./instrumentHost.d";
+import { Investigation } from "./investigation.d";
+import { Target } from "./target.d";
+
+export type { Instrument, InstrumentHost, Investigation, Target }
