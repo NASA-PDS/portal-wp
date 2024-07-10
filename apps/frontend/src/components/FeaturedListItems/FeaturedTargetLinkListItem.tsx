dbb344921ac9ebd76aaa1011754e62719fb2790a
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-07-10 10:24:44 -0700

    Improved responsiveness of Featured Target List Item component.

--- apps/frontend/src/components/FeaturedListItems/FeaturedTargetLinkListItem.tsx
+++ apps/frontend/src/components/FeaturedListItems/FeaturedTargetLinkListItem.tsx
@@ -6,7 +6,7 @@ import Divider from '@mui/material/Divider';
 import Stack from '@mui/material/Stack';
 import ArrowForward from '@mui/icons-material/ArrowForward';
 import { useState } from 'react';
-import { Link as AnchorLink } from '@mui/material';
+import { Link } from '@mui/material';
 import { Tag } from '@nasapds/wds-react';
 import { ellipsisText } from 'src/utils/strings';
 
@@ -38,14 +38,16 @@ export const FeaturedTargetLinkListItem = ({
   
   return (
     <Stack sx={{
-      marginTop: "15px",
+      marginTop: "15px"
     }}>
       <Box sx={{ width: "100%"}}>
         <Grid container spacing={2} sx={{
-          minHeight: "64px",
-          paddingY: "10px",
-          alignItems: "center"
-        }}>
+            minHeight: "64px",
+            paddingY: "10px",
+            alignItems: "center"
+          }}
+          display={{xs: "none", md: "flex"}}
+        >
           <Grid item xs={1}>
             <IconButton onClick={toggleDetails}>
               {
@@ -60,11 +62,10 @@ export const FeaturedTargetLinkListItem = ({
                     <path fillRule="evenodd" clipRule="evenodd" d="M11.0552 10L7.80005 6.76716L8.57249 6L12.6 10L8.57249 14L7.80005 13.2328L11.0552 10Z" fill="black"/>
                   </svg>
               }
-
             </IconButton>
           </Grid>
           <Grid item xs={4}>
-            <AnchorLink component="button" onClick={() => primaryAction()}
+            <Link component="button" onClick={() => primaryAction()}
               sx={{
                 textAlign: "left",
                 color: "#000000",
@@ -81,7 +82,7 @@ export const FeaturedTargetLinkListItem = ({
               }}>
                 {title}
               </Typography>
-            </AnchorLink>
+            </Link>
           </Grid>
           <Grid item xs={5}>
             <Typography
@@ -98,21 +99,17 @@ export const FeaturedTargetLinkListItem = ({
                 {ellipsisText(description, 256)}
             </Typography>
           </Grid>
-          <Grid item xs={2} sx={{textAlign:"right"}}>
-            <Grid container spacing={1} sx={{alignItems: "center", width: "fit-content"}}>
-              <Grid item>
+          <Grid item xs={2}>
+            <Stack direction="row">
                 <Typography sx={{
                   color: 'black',
                   fontSize: 16,
                   fontFamily: 'Inter',
                   fontWeight: '600',
                   lineHeight: '20px',
-                  textAlign: "left",
                   wordWrap: "break-word"
                 }}
                 >View Targets<br />and Data</Typography>
-              </Grid>
-              <Grid item>
                 <IconButton
                   sx={{
                     "&:hover": {
@@ -130,14 +127,93 @@ export const FeaturedTargetLinkListItem = ({
                     }}
                   />
                 </IconButton>
+                </Stack>
               </Grid>
             </Grid>
-          </Grid>
-        </Grid>
-        <Divider sx={{
-          marginTop: "15px",
-        }}/>
       </Box>
+      <Stack
+        sx={{
+          padding: "20px 20px 20px 0px",
+        }}
+        display={{xs: "flex", md: "none"}}
+        direction={"row"}
+        alignItems={"top"}
+      >
+        <Box sx={{height: "100%"}}>
+          <IconButton onClick={toggleDetails}>
+            {
+              showDetails ?
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="10" cy="10.0004" r="9.5" stroke="black"/>
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M10 11.0555L13.2328 7.80042L14 8.57286L10 12.6004L6 8.57286L6.76716 7.80042L10 11.0555Z" fill="black"/>
+                </svg>
+              :
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="10" cy="10" r="9.5" transform="rotate(-90 10 10)" stroke="black"/>
+                  <path fillRule="evenodd" clipRule="evenodd" d="M11.0552 10L7.80005 6.76716L8.57249 6L12.6 10L8.57249 14L7.80005 13.2328L11.0552 10Z" fill="black"/>
+                </svg>
+            }
+          </IconButton>
+                  </Box>
+          <Stack alignItems="left" gap={1} sx={{
+            paddingLeft: "15px",
+            marginBottom: "8px",
+          }}
+        >
+          <Link component="button" onClick={() => primaryAction()}
+            sx={{
+              textAlign: "left",
+              color: "#000000",
+              textDecoration: "none",
+              cursor: "pointer"
+            }}>
+            <Typography variant="subtitle2" display="inline">
+              {title}
+            </Typography>
+          </Link>
+
+          <Typography variant="body1" gutterBottom
+            sx={{
+              fontFamily: "Inter",
+              fontSize: "12px",
+              lineHeight: "130%",
+              fontWeight: "500"
+            }}
+          >
+            {ellipsisText(description, 256)}
+          </Typography>
+
+          <Stack direction="row" alignItems={"center"} gap={1}>
+            <Typography sx={{
+              color: 'black',
+              fontSize: 16,
+              fontFamily: 'Inter',
+              fontWeight: '600',
+              lineHeight: '20px',
+              wordWrap: "break-word"
+            }}
+            >View Targets and Data</Typography>
+            <IconButton
+              sx={{
+                "&:hover": {
+                  backgroundColor: "#B60109"
+                },
+                backgroundColor: "#F64137",
+                height: "20px",
+                width: "20px",
+                padding: "10px"
+              }}
+              aria-label="arrow"
+              onClick={() => {primaryAction()}}>
+                <ArrowForward sx={{
+                  color: "#FFFFFF",
+                  width: "14px"
+                }}
+              />
+            </IconButton>
+          </Stack>
+        </Stack>
+      </Stack>
       { showDetails ?
         <Box sx={{
           backgroundColor: "#F6F6F6",
@@ -207,6 +283,7 @@ export const FeaturedTargetLinkListItem = ({
         </Box>
         : ""
       }
+      <Divider />
     </Stack>
   );
 }
