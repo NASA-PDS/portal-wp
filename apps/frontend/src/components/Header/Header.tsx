5c60e4662708a4eb7c422afd373e65169508cd1d
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-07-31 10:32:55 -0700

    Cleaned up header nav items.

--- apps/frontend/src/components/Header/Header.tsx
+++ apps/frontend/src/components/Header/Header.tsx
@@ -10,17 +10,7 @@ const navItems = [
         id: "find-data-investigations",
         label: "Investigations",
         href: "/investigations",
-      },
-      /*{
-        id: "find-data-1",
-        label: "Find Data Link 1",
-        href: "/find-data/find-data-link-1",
-      },
-      {
-        id: "find-data-2",
-        label: "Find Data Link 2",
-        href: "/find-data/find-data-link-2",
-      },*/
+      }
     ],
   },
   {
@@ -76,23 +66,6 @@ const navItems = [
     id: "about",
     label: "About",
     href: "/about",
-    items: [
-      {
-        id: "about-link-1",
-        label: "About Link 1",
-        href: "/about/about-link-1",
-      },
-      {
-        id: "about-link-2",
-        label: "About Link 2",
-        href: "/about/about-link-2",
-      },
-      {
-        id: "about-link-3",
-        label: "About Link 3",
-        href: "/about/about-link-2",
-      },
-    ],
   },
 ];
 
