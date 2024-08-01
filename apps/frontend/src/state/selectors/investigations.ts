2aeb6f5bbe95f5696672e936655d763e1ab734e7
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-08-01 05:22:19 -0700

    Improved efficiency of filtering investigations on investigations directory page.

--- apps/frontend/src/state/selectors/investigations.ts
+++ apps/frontend/src/state/selectors/investigations.ts
@@ -88,8 +88,80 @@ export const selectFilteredInvestigations = createSelector(
   [ selectLatestVersionInvestigations, selectLatestVersionTargets, selectLatestVersionInstruments, selectSearchFilters ],
   ( latestInvestigations, latestTargets, latestInstruments, searchFilters ) => {
 
-    // Sort investigations alphabetically by title
-    latestInvestigations.sort( (a:Investigation,b:Investigation) => {
+    let filteredInvestigations = latestInvestigations;
+
+    // Search Filters are undefined, so return full list of investigations
+    if( searchFilters !== undefined ) {
+      
+      let foundTargetLids:string[] = [];
+      let foundInstrumentLids:Instrument[] = [];
+      if( searchFilters.freeText !== undefined ) {
+  
+        // Get LIDS for targets that match free-text search filter
+        foundTargetLids = latestTargets.filter( (target) => {
+          return (
+            target[PDS4_INFO_MODEL.TARGET.NAME]?.toLowerCase().includes(searchFilters?.freeText.toLowerCase() || "")
+            ||
+            target[PDS4_INFO_MODEL.LID]?.toLowerCase().includes(searchFilters?.freeText.toLowerCase() || "")
+          )
+        }).map( (target) => { return target[PDS4_INFO_MODEL.LID]});
+  
+        // Get LIDS for instruments that match free-text search filter
+        foundInstrumentLids = latestInstruments.filter( (instrument) => {
+          return (
+            instrument[PDS4_INFO_MODEL.INSTRUMENT.NAME]?.toLowerCase().includes(searchFilters?.freeText.toLowerCase() || "")
+            ||
+            instrument[PDS4_INFO_MODEL.LID]?.toLowerCase().includes(searchFilters?.freeText.toLowerCase() || "")
+          )
+        });
+      }
+  
+      // Get LIDS for instruments hosts of those that matched free-text search filter
+      const foundInstrumentHostLids:string[] = [];
+      foundInstrumentLids.forEach( (instrument:Instrument) => {
+        instrument[PDS4_INFO_MODEL.REF_LID_INSTRUMENT_HOST]?.forEach( (instrumentHost) => {
+          if( !foundInstrumentHostLids.includes(instrumentHost) ) {
+            foundInstrumentHostLids.push(instrumentHost);
+          }
+        })
+      });
+  
+      const targetInvestigationLids = latestInvestigations.filter( (investigation:Investigation) => {
+        return investigation[PDS4_INFO_MODEL.REF_LID_TARGET]?.filter( (investigationTarget) => {
+          return foundTargetLids.includes(investigationTarget);
+        }).length > 0
+      }).map( (investigation:Investigation) => { return investigation[PDS4_INFO_MODEL.LID]});
+  
+      const instrumentHostInvestigationLids = latestInvestigations.filter( (investigation:Investigation) => {
+        return investigation[PDS4_INFO_MODEL.REF_LID_INSTRUMENT_HOST]?.filter( (investigationInstrumentHost) => {
+          return foundInstrumentHostLids.includes(investigationInstrumentHost);
+        }).length > 0
+      }).map( (investigation:Investigation) => { return investigation[PDS4_INFO_MODEL.LID]});
+  
+      filteredInvestigations = latestInvestigations.filter(
+        (investigation) => {
+          return (
+            (
+              searchFilters.freeText === undefined 
+              || investigation[PDS4_INFO_MODEL.TITLE].toLowerCase().includes(searchFilters?.freeText.toLowerCase() || "")
+              || investigation[PDS4_INFO_MODEL.LID].toLowerCase().includes(searchFilters?.freeText.toLowerCase() || "")
+              || targetInvestigationLids.includes(investigation[PDS4_INFO_MODEL.LID])
+              || instrumentHostInvestigationLids.includes(investigation[PDS4_INFO_MODEL.LID])
+            )
+            &&
+            ( 
+              searchFilters.type === undefined 
+              || searchFilters.type === INVESTIGATION_TYPE.ALL 
+              || searchFilters.type === investigation[PDS4_INFO_MODEL.INVESTIGATION.TYPE] 
+            )
+          )
+        }
+      );
+
+    }
+
+    // Sort investigations alphabetically by title and then return the list
+    return filteredInvestigations.sort( (a:Investigation,b:Investigation) => {
       if( a[PDS4_INFO_MODEL.TITLE].toLowerCase() < b[PDS4_INFO_MODEL.TITLE].toLowerCase() ) {
         return -1
       } else if( a[PDS4_INFO_MODEL.TITLE].toLowerCase() > b[PDS4_INFO_MODEL.TITLE].toLowerCase() ) {
@@ -97,76 +169,6 @@ export const selectFilteredInvestigations = createSelector(
       }
       return 0;
     });
-
-    // Search Filters are undefined, so return full list of investigations
-    if( searchFilters === undefined ) {
-      return latestInvestigations;
-    }
-
-    let foundTargetLids:string[] = [];
-    let foundInstrumentLids:Instrument[] = [];
-    if( searchFilters.freeText !== undefined ) {
-
-      // Get LIDS for targets that match free-text search filter
-      foundTargetLids = latestTargets.filter( (target) => {
-        return (
-          target[PDS4_INFO_MODEL.TARGET.NAME]?.toLowerCase().includes(searchFilters?.freeText.toLowerCase() || "")
-          ||
-          target[PDS4_INFO_MODEL.LID]?.toLowerCase().includes(searchFilters?.freeText.toLowerCase() || "")
-        )
-      }).map( (target) => { return target[PDS4_INFO_MODEL.LID]});
-
-      // Get LIDS for instruments that match free-text search filter
-      foundInstrumentLids = latestInstruments.filter( (instrument) => {
-        return (
-          instrument[PDS4_INFO_MODEL.INSTRUMENT.NAME]?.toLowerCase().includes(searchFilters?.freeText.toLowerCase() || "")
-          ||
-          instrument[PDS4_INFO_MODEL.LID]?.toLowerCase().includes(searchFilters?.freeText.toLowerCase() || "")
-        )
-      });
-    }
-
-    // Get LIDS for instruments hosts of those that matched free-text search filter
-    const foundInstrumentHostLids:string[] = [];
-    foundInstrumentLids.forEach( (instrument:Instrument) => {
-      instrument[PDS4_INFO_MODEL.REF_LID_INSTRUMENT_HOST]?.forEach( (instrumentHost) => {
-        if( !foundInstrumentHostLids.includes(instrumentHost) ) {
-          foundInstrumentHostLids.push(instrumentHost);
-        }
-      })
-    });
-
-    const targetInvestigationLids = latestInvestigations.filter( (investigation:Investigation) => {
-      return investigation[PDS4_INFO_MODEL.REF_LID_TARGET]?.filter( (investigationTarget) => {
-        return foundTargetLids.includes(investigationTarget);
-      }).length > 0
-    }).map( (investigation:Investigation) => { return investigation[PDS4_INFO_MODEL.LID]});
-
-    const instrumentHostInvestigationLids = latestInvestigations.filter( (investigation:Investigation) => {
-      return investigation[PDS4_INFO_MODEL.REF_LID_INSTRUMENT_HOST]?.filter( (investigationInstrumentHost) => {
-        return foundInstrumentHostLids.includes(investigationInstrumentHost);
-      }).length > 0
-    }).map( (investigation:Investigation) => { return investigation[PDS4_INFO_MODEL.LID]});
-
-    return latestInvestigations.filter(
-      (investigation) => {
-        return (
-          (
-            searchFilters.freeText === undefined 
-            || investigation[PDS4_INFO_MODEL.TITLE].toLowerCase().includes(searchFilters?.freeText.toLowerCase() || "")
-            || investigation[PDS4_INFO_MODEL.LID].toLowerCase().includes(searchFilters?.freeText.toLowerCase() || "")
-            || targetInvestigationLids.includes(investigation[PDS4_INFO_MODEL.LID])
-            || instrumentHostInvestigationLids.includes(investigation[PDS4_INFO_MODEL.LID])
-          )
-          &&
-          ( 
-            searchFilters.type === undefined 
-            || searchFilters.type === INVESTIGATION_TYPE.ALL 
-            || searchFilters.type === investigation[PDS4_INFO_MODEL.INVESTIGATION.TYPE] 
-          )
-        )
-      }
-    );
 
   }
 )
