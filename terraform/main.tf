95f61e01c259c237d14307390eb241dc35cd5332
Sean Kelly <kelly@seankelly.biz>
2024-09-05 18:11:35 -0500

    Thanks to @sjoshi-jpl for this!

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,9 +16,14 @@
 # Required Terraform version.
 
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">=1.2.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0.0"
+    }
+  }
 }
-
 
 # AWS Provision
 # =============
@@ -29,19 +34,28 @@ provider "aws" {
   region = "us-west-2"
 }
 
+# S3 Bucket
+# =========
+#
+# Since Node.js neatly distributes itself into a set of static files, we can
+# make the app available over a web server in front of S3. So, let's define
+# the S3 bucket.
+
+resource "aws_s3_bucket" "vite_app_bucket" {
+  bucket = "pds-en"
+}
 
 # S3 Uploading
 # ============
 #
 # After building the project with npm we upload the files from `dist`.
 
-resource "aws_s3_bucket_object" "vite_app_files" {
-  for_each = fileset("../apps/frontend/dist", "**")
+resource "aws_s3_object" "vite_app_files" {
+  for_each = fileset("../dist", "**")
 
-  bucket = "pds-portal-demo"
+  bucket = aws_s3_bucket.vite_app_bucket.id
   key    = each.value
-  source = "${path.module}/../apps/frontend/dist/${each.value}"
-  acl    = "public-read"
+  source = "${path.module}/../dist/${each.value}"
 }
 
 
@@ -51,16 +65,23 @@ resource "aws_s3_bucket_object" "vite_app_files" {
 # Make sure the files are readable by the public internet.
 
 resource "aws_s3_bucket_policy" "vite_app_bucket_policy" {
-  bucket = "pds-portal-demo"
+  bucket = aws_s3_bucket.vite_app_bucket.id
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action    = "s3:GetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Resource  = "arn:aws:s3:::pds-portal-demo/*"
+        Action = "s3:GetObject"
+        Effect = "Allow"
+        Principal = {
+          "Service" = "cloudfront.amazonaws.com"
+        }
+        Resource = "${aws_s3_bucket.vite_app_bucket.arn}/*"
+        Condition = {
+          StringEquals = {
+            "aws:SourceArn" = "arn:aws:cloudfront::441083951559:distribution/E18VQ4K51WT0LV"
+          }
+        }
       }
     ]
   })
