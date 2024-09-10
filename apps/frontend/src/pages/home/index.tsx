6e7137428bc424f5cfb540dea09fcdc1dce9db6b
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-09-10 16:38:02 -0700

    Added dummy data to homepage to render ExploreTopicsSixUp module.

--- apps/frontend/src/pages/home/index.tsx
+++ apps/frontend/src/pages/home/index.tsx
@@ -1,3 +1,5 @@
+import { Stack } from "@mui/material";
+import { Card } from "@nasapds/wds-react";
 import { DocumentMeta } from "src/components/DocumentMeta/DocumentMeta";
 import { ExploreTopicsSixUp } from "src/components/ExploreTopicsSixUp";
 
@@ -11,7 +13,21 @@ const HomePage = () => {
       tabs={[
         {
           label: "Investigations",
-          content: <>Investigation's Content</>
+          content: <>
+            <Stack direction={"row"} gap={"12px"}>
+              {
+                [
+                  { "title": "Mars 2020" },
+                  { "title": "Insight" },
+                  { "title": "Cassini" },
+                  { "title": "Juno" },
+                  { "title": "Mars Science Laboratory" },
+                ].map( (investigation) => {
+                  return <Card title={investigation.title} height={250} width={204} maxWidth={204} />
+                })
+              }
+            </Stack>
+          </>
         },
         {
           label: "Instruments",
