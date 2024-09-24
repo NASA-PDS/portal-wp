820d52143e0d072d2ebac5e3b2028329b970d358
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-09-24 11:59:43 -0700

    Optimized ExploreTopicsSixUp declaration for improved reusability.

--- apps/frontend/src/components/ExploreTopicsSixUp/ExploreTopicsSixUp.tsx
+++ apps/frontend/src/components/ExploreTopicsSixUp/ExploreTopicsSixUp.tsx
@@ -1,11 +1,11 @@
-import { Container } from "@mui/material"
-import { Typography } from "@nasapds/wds-react"
-import { Tabs } from "@nasapds/wds-react";
+import { Box, Container, Grid, Stack } from "@mui/material";
+import { Card, ButtonCard, MediaCard, Tabs, Typography } from "@nasapds/wds-react"
+import { Link } from "react-router-dom";
 
 import "./ExploreTopicsSixUp.scss";
 
 export type ExploreTopicsSixUpProps = {
-  tabs:Array<{label:string, content:React.ReactElement}>;
+  tabs:Array<{label:string, content:{jumpLinkLabel:string, jumpLinkUrl:string, cards:Array<{image:string, imageDescription:string, lid:string, title:string, url:string}>}}>;
   tabsDescription:string;
   title:string;
 }
@@ -25,7 +25,53 @@ export const ExploreTopicsSixUp = ({
         className="pds-explore-topics-six-up-container"
         >
           <Typography variant="h2" weight="bold" className="pds-explore-topics-six-up-title">{title}</Typography>
-          <Tabs description={tabsDescription} tabs={tabs} />
+          <Tabs description={tabsDescription} tabs={
+            tabs.map( (tab, tabIndex) => {
+              return {
+                label: tab.label,
+                content: <Box key={tabIndex}>
+                  <Stack direction={"row"} spacing={"12px"} style={{overflowX: "scroll", scrollSnapType: "x mandatory"}} display={{md: "none"}}>
+                    {
+                      tab.content.cards.map( (card, cardIndex) => {
+                        return <>
+                          <Card 
+                            height={250}
+                            image={card.image}
+                            imageDescription={card.imageDescription}
+                            maxWidth={204} 
+                            title={card.title}
+                            url={card.url} 
+                            width={204}
+                            style={{scrollSnapAlign: "start"}}
+                            key={cardIndex}
+                          />
+                        </>
+                      })
+                    }
+                    <Link to={tab.content.jumpLinkUrl}><ButtonCard title={tab.content.jumpLinkLabel} width={"204px"}/></Link>
+                  </Stack>
+                  <Grid container spacing={"12px"} display={{xs: "none", md: "flex"}}>
+                    {
+                      tab.content.cards.map( (card, cardIndex) => {
+                        return <>
+                          <Grid item xs={12} sm={6} md={4} lg={2} key={cardIndex}>
+                            <Link to={card.url}>
+                              <MediaCard 
+                                image={card.image}
+                                imageDescription={card.imageDescription}
+                                title={card.title}
+                              />
+                            </Link>
+                          </Grid>
+                        </>
+                      })
+                    }
+                    <Grid item md={4} lg={2}><Link to={tab.content.jumpLinkUrl}><ButtonCard title={tab.content.jumpLinkLabel} /></Link></Grid>
+                  </Grid>
+                </Box>
+              }
+            })
+          } />
       </Container>
     </Container>
   </>
--- apps/frontend/src/pages/home/index.tsx
+++ apps/frontend/src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import { Grid, Stack } from "@mui/material";
-import { Card, ButtonCard, MediaCard } from "@nasapds/wds-react";
+import { Card, MediaCard } from "@nasapds/wds-react";
 import { DocumentMeta } from "src/components/DocumentMeta/DocumentMeta";
-import { ExploreTopicsSixUp } from "src/components/ExploreTopicsSixUp";
+import { ExploreTopicsSixUp, ExploreTopicsSixUpProps } from "src/components/ExploreTopicsSixUp";
 import { SubmitDataQuickLinks } from "src/components/SubmitDataQuickLinks";
 import { ExploreTopicsFourUp } from "src/components/ExploreTopicsFourUp";
 import { Link } from "react-router-dom";
@@ -10,81 +10,100 @@ import { HomeSearch } from "src/components/HomeSearch/HomeSearch";
 
 const HomePage = () => {
 
-  const investigations = [
-    {
-      image: "/assets/images/cards/missions/urn--nasa--pds--context--investigation--mission---mars2020.jpg",
-      imageDescription: "Artist's rendering of the Perserverance rover on the Mars surface.",
-      lid: "urn--nasa--pds--context--investigation--mission---mars2020",
-      title: "Mars 2020",
-      url: "/investigations/urn--nasa--pds--context--investigation--mission---mars2020/instruments"
-    },
-    {
-      image: "/assets/images/cards/missions/urn--nasa--pds--context--investigation--mission---insight.jpg",
-      imageDescription: "Artist's rendering of the Insight Lander on the Mars surface.",
-      lid: "urn--nasa--pds--context--investigation--mission---insight",
-      title: "Insight",
-      url: "/investigations/urn--nasa--pds--context--investigation--mission---insight/instruments"
-    },
-    {
-      image: "/assets/images/cards/missions/urn--nasa--pds--context--investigation--mission---cassini-huygens.jpg",
-      imageDescription: "Artist's rendering of the Cassini spacecraft.",
-      lid: "urn--nasa--pds--context--investigation--mission---cassini-huygens",
-      title: "Cassini",
-      url: "/investigations/urn--nasa--pds--context--investigation--mission---cassini-huygens/instruments"
-    },
-    {
-      image: "/assets/images/cards/missions/urn--nasa--pds--context--investigation--mission---juno.jpg",
-      imageDescription: "Artist's rendering of the Juno spacecraft with Jupiter in the background.",
-      lid: "urn--nasa--pds--context--investigation--mission---juno",
-      title: "Juno",
-      url: "/investigations/urn--nasa--pds--context--investigation--mission---juno/instruments"
-    },
-    {
-      image: "/assets/images/cards/missions/urn--nasa--pds--context--investigation--mission---mars----science----laboratory.jpg",
-      imageDescription: "Artist's rendering of the Curiosity rover on the Mars surface.",
-      lid: "urn--nasa--pds--context--investigation--mission---mars----science----laboratory",
-      title: "Mars Science Laboratory",
-      url: "/investigations/urn--nasa--pds--context--investigation--mission---mars----science----laboratory/instruments"
-    },
-  ];
-
-  const instruments = [
-    {
-      image: undefined,
-      imageDescription: "",
-      lid: "",
-      title: "Near Infrared Camera (NIRCam)",
-      url: ""
-    },
-    {
-      image: "/assets/images/cards/instruments/urn--nasa--pds--context--instrument--mars2020---sherloc.jpg",
-      imageDescription: "Picture of the Scanning Habitable Environments with Raman & Luminescence for Organics & Chemicals instrument in a clean room.",
-      lid: "urn--nasa--pds--context--instrument--mars2020---sherloc",
-      title: "Scanning Habitable Environments with Raman & Luminescence for Organics & Chemicals (SHERLOC)",
-      url: "/instruments/urn--nasa--pds--context--instrument--mars2020---sherloc/data"
-    },
-    {
-      image: "/assets/images/cards/instruments/urn--nasa--pds--context--instrument--ovirs---orex.jpg",
-      imageDescription: "Picture of the OSIRIS-REx Visible and Infrared Spectrometer (OVIRS) instrument.",
-      lid: "urn--nasa--pds--context--instrument--ovirs---orex",
-      title: "OSIRIS-REx Visible and Infrared Spectrometer (OVIRS)",
-      url: "/instruments/urn--nasa--pds--context--instrument--ovirs---orex/data"
-    },
-    {
-      image: "/assets/images/cards/instruments/urn--nasa--pds--context--instrument--mount----bigelow---1m54---mont4k.jpg",
-      imageDescription: "Picture of the Mont4K CCD Imager instrument.",
-      lid: "urn--nasa--pds--context--instrument--mount----bigelow---1m54---mont4k",
-      title: "Mont4K CCD Imager",
-      url: "/instruments/urn--nasa--pds--context--instrument--mount----bigelow---1m54---mont4k/data"
-    },
-    {
-      image: "/assets/images/cards/instruments/urn--nasa--pds--context--instrument--chemcam----libs---msl.jpg",
-      imageDescription: "Picture of the Chemistry Camera Laser Induced Breakdown Spectrometer (ChemCam) instrument.",
-      lid: "urn--nasa--pds--context--instrument--chemcam----libs---msl",
-      title: "Chemistry Camera Laser Induced Breakdown Spectrometer (ChemCam)",
-      url: "/instruments/urn--nasa--pds--context--instrument--chemcam----libs---msl/data"
-    },
-  ];
+  const exploreSixUpTabs:ExploreTopicsSixUpProps = {
+    tabsDescription: "Explore the PDS Data Archive by browsing data by the categories they are orgnized within.",
+    title: "Explore our Data Archive",
+    tabs: [
+      {
+        label: "Investigations",
+        content: {
+          jumpLinkLabel: "More Investigations",
+          jumpLinkUrl: "/investigations",
+          cards: [
+            {
+              image: "/assets/images/cards/missions/urn--nasa--pds--context--investigation--mission---mars2020.jpg",
+              imageDescription: "Artist's rendering of the Perserverance rover on the Mars surface.",
+              lid: "urn--nasa--pds--context--investigation--mission---mars2020",
+              title: "Mars 2020",
+              url: "/investigations/urn--nasa--pds--context--investigation--mission---mars2020/instruments"
+            },
+            {
+              image: "/assets/images/cards/missions/urn--nasa--pds--context--investigation--mission---insight.jpg",
+              imageDescription: "Artist's rendering of the Insight Lander on the Mars surface.",
+              lid: "urn--nasa--pds--context--investigation--mission---insight",
+              title: "Insight",
+              url: "/investigations/urn--nasa--pds--context--investigation--mission---insight/instruments"
+            },
+            {
+              image: "/assets/images/cards/missions/urn--nasa--pds--context--investigation--mission---cassini-huygens.jpg",
+              imageDescription: "Artist's rendering of the Cassini spacecraft.",
+              lid: "urn--nasa--pds--context--investigation--mission---cassini-huygens",
+              title: "Cassini",
+              url: "/investigations/urn--nasa--pds--context--investigation--mission---cassini-huygens/instruments"
+            },
+            {
+              image: "/assets/images/cards/missions/urn--nasa--pds--context--investigation--mission---juno.jpg",
+              imageDescription: "Artist's rendering of the Juno spacecraft with Jupiter in the background.",
+              lid: "urn--nasa--pds--context--investigation--mission---juno",
+              title: "Juno",
+              url: "/investigations/urn--nasa--pds--context--investigation--mission---juno/instruments"
+            },
+            {
+              image: "/assets/images/cards/missions/urn--nasa--pds--context--investigation--mission---mars----science----laboratory.jpg",
+              imageDescription: "Artist's rendering of the Curiosity rover on the Mars surface.",
+              lid: "urn--nasa--pds--context--investigation--mission---mars----science----laboratory",
+              title: "Mars Science Laboratory",
+              url: "/investigations/urn--nasa--pds--context--investigation--mission---mars----science----laboratory/instruments"
+            },
+          ]
+        }
+      }, 
+      {
+        label: "Instruments",
+        content: {
+          jumpLinkLabel: "More Instruments",
+          jumpLinkUrl: "/instruments",
+          cards: [
+            {
+              image: "",
+              imageDescription: "",
+              lid: "",
+              title: "Near Infrared Camera (NIRCam)",
+              url: ""
+            },
+            {
+              image: "/assets/images/cards/instruments/urn--nasa--pds--context--instrument--mars2020---sherloc.jpg",
+              imageDescription: "Picture of the Scanning Habitable Environments with Raman & Luminescence for Organics & Chemicals instrument in a clean room.",
+              lid: "urn--nasa--pds--context--instrument--mars2020---sherloc",
+              title: "Scanning Habitable Environments with Raman & Luminescence for Organics & Chemicals (SHERLOC)",
+              url: "/instruments/urn--nasa--pds--context--instrument--mars2020---sherloc/data"
+            },
+            {
+              image: "/assets/images/cards/instruments/urn--nasa--pds--context--instrument--ovirs---orex.jpg",
+              imageDescription: "Picture of the OSIRIS-REx Visible and Infrared Spectrometer (OVIRS) instrument.",
+              lid: "urn--nasa--pds--context--instrument--ovirs---orex",
+              title: "OSIRIS-REx Visible and Infrared Spectrometer (OVIRS)",
+              url: "/instruments/urn--nasa--pds--context--instrument--ovirs---orex/data"
+            },
+            {
+              image: "/assets/images/cards/instruments/urn--nasa--pds--context--instrument--mount----bigelow---1m54---mont4k.jpg",
+              imageDescription: "Picture of the Mont4K CCD Imager instrument.",
+              lid: "urn--nasa--pds--context--instrument--mount----bigelow---1m54---mont4k",
+              title: "Mont4K CCD Imager",
+              url: "/instruments/urn--nasa--pds--context--instrument--mount----bigelow---1m54---mont4k/data"
+            },
+            {
+              image: "/assets/images/cards/instruments/urn--nasa--pds--context--instrument--chemcam----libs---msl.jpg",
+              imageDescription: "Picture of the Chemistry Camera Laser Induced Breakdown Spectrometer (ChemCam) instrument.",
+              lid: "urn--nasa--pds--context--instrument--chemcam----libs---msl",
+              title: "Chemistry Camera Laser Induced Breakdown Spectrometer (ChemCam)",
+              url: "/instruments/urn--nasa--pds--context--instrument--chemcam----libs---msl/data"
+            },
+          ]
+        }
+      }
+    ]
+  };
 
   const toolsAndServices = [
     {
@@ -123,96 +142,7 @@ const HomePage = () => {
       description={ "The Planetary Data Systems Homepage" }
     />
     <HomeSearch />
-    <ExploreTopicsSixUp
-      tabs={[
-        {
-          label: "Investigations",
-          content: <>
-            <Stack direction={"row"} spacing={"12px"} style={{overflowX: "scroll", scrollSnapType: "x mandatory"}} display={{md: "none"}}>
-              {
-                investigations.map( (investigation) => {
-                  return <>
-                    <Card 
-                      height={250}
-                      image={investigation.image}
-                      imageDescription={investigation.imageDescription}
-                      maxWidth={204} 
-                      title={investigation.title}
-                      url={investigation.url} 
-                      width={204}
-                      style={{scrollSnapAlign: "start"}}
-                    />
-                  </>
-                })
-              }
-              <Link to={"/investigations"}><ButtonCard title={"More Investigations"} width={"204px"}/></Link>
-            </Stack>
-            <Grid container spacing={"12px"} display={{xs: "none", md: "flex"}}>
-              {
-                investigations.map( (investigation) => {
-                  return <>
-                    <Grid item xs={12} sm={6} md={4} lg={2}>
-                      <Link to={investigation.url}>
-                        <MediaCard 
-                          image={investigation.image}
-                          imageDescription={investigation.imageDescription}
-                          title={investigation.title}
-                        />
-                      </Link>
-                    </Grid>
-                  </>
-                })
-              }
-              <Grid item md={4} lg={2}><Link to={"/investigations"}><ButtonCard title={"More Investigations"} /></Link></Grid>
-            </Grid>
-          </>
-        },
-        {
-          label: "Instruments",
-          content: <>
-            <Stack direction={"row"} spacing={"12px"} style={{overflowX: "scroll", scrollSnapType: "x mandatory"}} display={{md: "none"}}>
-              {
-                instruments.map( (instrument) => {
-                  return <>
-                    <Card 
-                      height={250}
-                      image={instrument.image}
-                      imageDescription={instrument.imageDescription}
-                      maxWidth={204} 
-                      title={instrument.title}
-                      url={instrument.url} 
-                      width={204}
-                      style={{scrollSnapAlign: "start"}}
-                    />
-                  </>
-                })
-              }
-              <Link to={"/instruments"}><ButtonCard title={"More Instruments"} width={"204px"}/></Link>
-            </Stack>
-            <Grid container spacing={"12px"} display={{xs: "none", md: "flex"}}>
-              {
-                instruments.map( (instrument) => {
-                  return <>
-                    <Grid item xs={12} sm={6} md={4} lg={2}>
-                      <Link to={instrument.url}>
-                        <MediaCard 
-                          image={instrument.image}
-                          imageDescription={instrument.imageDescription}
-                          title={instrument.title}
-                        />
-                      </Link>
-                    </Grid>
-                  </>
-                })
-              }
-              <Grid item md={4} lg={2}><Link to={"/instruments"}><ButtonCard title={"More Instruments"} /></Link></Grid>
-            </Grid>
-          </>
-        }
-      ]}
-      tabsDescription="Explore the PDS Data Archive by browsing data by the categories they are orgnized within."
-      title={"Explore our Data Archive"} 
-    />
+    <ExploreTopicsSixUp {...exploreSixUpTabs} />
     <SubmitDataQuickLinks />
     <ExploreTopicsFourUp
       title="Explore Tools and Services"
