bbedd0488fa8906a604d0f9cf6692c9a5a00ccef
Eddie Arevalo <eddiesarevalo@gmail.com>
2024-10-09 12:37:35 -0700

    Update Footer Last Update Date
    -Updated last updated date in the footer.

--- apps/frontend/src/components/Footer/Footer.tsx
+++ apps/frontend/src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import { FooterLink, Footer } from "@nasapds/wds-react";
 
 function PortalFooter() {
 
-  const pageLastUpdated="Sept. 19, 2024"
+  const pageLastUpdated="Oct. 10, 2024"
 
   const primaryLinks:FooterLink[] = [
     {
