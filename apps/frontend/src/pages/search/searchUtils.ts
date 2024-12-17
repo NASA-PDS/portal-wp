a26eefd86e7d1dc6f61fe45815094d81fae53bc2
Eddie Arevalo <eddiesarevalo@gmail.com>
2024-12-17 13:26:16 -0800

    Add Solr String To String Array Flexibility #137
    -Added an expected type that the client will expect from solr. It will then convert all values to arrays and use that type instead.
    -This is so that as solr is being updated between strings or string arrays it won't break the client due to unexpected type.

--- apps/frontend/src/pages/search/searchUtils.ts
+++ apps/frontend/src/pages/search/searchUtils.ts
@@ -1,9 +1,20 @@
 import {
     IdentifierNameDoc,
+    IdentifierNameResponse,
+    Response,
     SolrSearchResponse,
     SolrIdentifierNameResponse,
     SearchResultDoc,
 } from "src/types/solrSearchResponse";
+
+import {
+    SolrSearchResponse as SolrSearchResponseExpected,
+    SolrIdentifierNameResponse as SolrIdentifierNameResponseExpected,
+    IdentifierNameResponse as IdentifierNameResponseExpected,
+    IdentifierNameDoc as IdentifierNameDocExpected,
+    Response as ResponseExpected,
+    SearchResultDoc as SearchResultDocExpected
+} from "src/types/solrSearchResponseExpected";
 
 export const calculatePaginationCount = (data: SolrSearchResponse) => {
     const rows = Number(data.responseHeader.params.rows);
@@ -80,14 +91,6 @@ export const formatFilterQueries = (filters: string) => {
     });
 
     return formattedFilterQueryString;
-};
-
-export const formatIdentifierNameResults = (data: SolrIdentifierNameResponse) => {
-    return data;
-};
-
-export const formatSearchResults = (data: SolrSearchResponse) => {
-    return data;
 };
 
 export const getDocType = (doc: SearchResultDoc) => {
@@ -227,7 +230,7 @@ const getUnmatchedFilterName = (
     }
 
     for(let i = 0; i < originals.length; i++){
-        if(originals[i].identifier === identifier){
+        if(originals[i].identifier[0] === identifier){
             if(parentName === "target_ref"){
                 return originals[i].target_name[0];
             }
@@ -306,7 +309,7 @@ export const mapFilterIdsToName = (ids: string[], names: IdentifierNameDoc[], se
           let name: string = "";
 
           if (nameDoc.title) {
-            name = nameDoc.title;
+            name = nameDoc.title[0];
           }
 
           filtersMap.push({
@@ -353,3 +356,196 @@ export const mapPageType = (ids: string[], searchResultFacets?: (number | string
 
     return filtersMap;
 };
+
+export const formatSearchResults = (data: SolrSearchResponseExpected) => {
+    const formattedData: SolrSearchResponse = transformSolrSearchResponseExpected(data);
+
+    return formattedData;
+};
+
+
+const transformSolrSearchResponseExpected = (data: SolrSearchResponseExpected) => {
+    const formattedData: SolrSearchResponse  = {
+        response: transformResponse(data.response),
+        responseHeader: data.responseHeader,
+        facet_counts: data.facet_counts
+    }
+
+    return formattedData;
+};
+
+
+const transformResponse = (data: ResponseExpected) => {
+    const formattedData: Response  = {
+        numFound: data.numFound,
+        start: data.start,
+        maxScore: data.maxScore,
+        docs: transformDocs(data.docs)
+    }
+
+    return formattedData;
+}
+
+const transformDocs = (data: SearchResultDocExpected[]) => {
+    const formattedData: SearchResultDoc[] = [];
+
+    data.forEach((docExpected) => {
+        const doc: SearchResultDoc = {
+            file_ref_location: convertToStringArray(docExpected.file_ref_location),
+            data_class:convertToStringArray(docExpected.data_class),
+            description: convertToStringArray(docExpected.description),
+            file_ref_url: convertToStringArray(docExpected.file_ref_url),
+            title: convertToStringArray(docExpected.title),
+            resLocation: convertToStringArray(docExpected.resLocation),
+            objectType: convertToStringArray(docExpected.objectType),
+            product_class: convertToStringArray(docExpected.product_class),
+            data_product_type: convertToStringArray(docExpected.data_product_type),
+            file_ref_size: convertToStringArray(docExpected.file_ref_size),
+            modification_date: convertToStringArray(docExpected.modification_date),
+            file_ref_name: convertToStringArray(docExpected.file_ref_name),
+            identifier: convertToStringArray(docExpected.identifier),
+            resource_url: convertToStringArray(docExpected.resource_url),
+            agency_name: convertToStringArray(docExpected.agency_name),
+            'form-agency': convertToStringArray(docExpected['form-agency']),
+            modification_description: convertToStringArray(docExpected.modification_description),
+            resource_type: convertToStringArray(docExpected.resource_type),
+            search_id: convertToStringArray(docExpected.search_id),
+            pds_model_version: convertToStringArray(docExpected.pds_model_version),
+            resource_description: convertToStringArray(docExpected.resource_description),
+            resource_name: convertToStringArray(docExpected.resource_name),
+            timestamp: convertToStringArray(docExpected.timestamp),
+            score: docExpected.score,
+            investigation_type: convertToStringArray(docExpected.investigation_type),
+            primary_result_purpose: convertToStringArray(docExpected.primary_result_purpose),
+            node_id: convertToStringArray(docExpected.node_id),
+            instrument_type: convertToStringArray(docExpected.instrument_type),
+            facility_country: convertToStringArray(docExpected.facility_country),
+            facility_type: convertToStringArray(docExpected.facility_type),
+            facility_description: convertToStringArray(docExpected.facility_description),
+            telescope_description: convertToStringArray(docExpected.telescope_description),
+            telescope_aperture: convertToStringArray(docExpected.telescope_aperture),
+            instrument_name: convertToStringArray(docExpected.instrument_name),
+            investigation_ref: convertToStringArray(docExpected.investigation_ref),
+            citation_publication_year: convertToStringArray(docExpected.citation_publication_year) 
+        }
+
+        if(docExpected.collection_type){
+            doc.collection_type = convertToStringArray(docExpected.collection_type);
+        }
+        if(docExpected.investigation_description){
+            doc.investigation_description = convertToStringArray(docExpected.investigation_description);
+        }
+        if(docExpected.instrument_host_name){
+            doc.instrument_host_name = convertToStringArray(docExpected.instrument_host_name);
+        }
+        if(docExpected.investigation_start_date){
+            doc.investigation_start_date = convertToStringArray(docExpected.investigation_start_date);
+        }
+        if(docExpected.investigation_stop_date){
+            doc.investigation_stop_date = convertToStringArray(docExpected.investigation_stop_date);
+        }
+        if(docExpected.instrument_description){
+            doc.instrument_description = convertToStringArray(docExpected.instrument_description);
+        }
+        if(docExpected['form-instrument-type']){
+            doc['form-instrument-type'] = convertToStringArray(docExpected['form-instrument-type']);
+        }
+        if(docExpected['form-instrument-host']){
+            doc['form-instrument-host'] = convertToStringArray(docExpected['form-instrument-host']);
+        }
+        if(docExpected['form-investigation']){
+            doc['form-investigation'] = convertToStringArray(docExpected['form-investigation']);
+        }
+        if(docExpected.citation_doi){
+            doc.citation_doi = convertToStringArray(docExpected.citation_doi);
+        }
+        if(docExpected.primary_result_processing_level){
+            doc.primary_result_processing_level = convertToStringArray(docExpected.primary_result_processing_level);
+        }
+        if(docExpected.observation_start_date_time){
+            doc.observation_start_date_time = convertToStringArray(docExpected.observation_start_date_time);
+        }
+        if(docExpected.observation_stop_date_time){
+            doc.observation_stop_date_time = convertToStringArray(docExpected.observation_stop_date_time);
+        }
+        if(docExpected.investigation_name){
+            doc.investigation_name = convertToStringArray(docExpected.investigation_name);
+        }
+        if(docExpected.primary_result_discipline_name){
+            doc.primary_result_discipline_name = convertToStringArray(docExpected.primary_result_discipline_name);
+        }
+        if(docExpected.target_type){
+            doc.target_type = convertToStringArray(docExpected.target_type);
+        }
+        if(docExpected.service_url){
+            doc.service_url = convertToStringArray(docExpected.service_url);
+        }
+        if(docExpected.version_id){
+            doc.version_id = convertToStringArray(docExpected.version_id);
+        }
+        if(docExpected.service_category){
+            doc.service_category = convertToStringArray(docExpected.service_category);
+        }
+        if(docExpected['form-target']){
+            doc['form-target'] = convertToStringArray(docExpected['form-target']);
+        }
+        if(docExpected.target_description){
+            doc.target_description = convertToStringArray(docExpected.target_description);
+        }
+        if(docExpected.instrument_host_description){
+            doc.instrument_host_description = convertToStringArray(docExpected.instrument_host_description);
+        }
+
+        formattedData.push(doc);
+    });
+
+    return formattedData;
+}
+
+export const formatIdentifierNameResults = (data: SolrIdentifierNameResponseExpected) => {
+    const formattedData: SolrIdentifierNameResponse = transformSolrIdentifierNameResponse(data);
+
+    return formattedData;
+};
+
+
+const transformSolrIdentifierNameResponse = (data: SolrIdentifierNameResponseExpected) => {
+    const formattedData: SolrIdentifierNameResponse  = {
+        response: transformIdentifierNameResponse(data.response),
+        responseHeader: data.responseHeader,
+        facet_counts: data.facet_counts
+    }
+
+    return formattedData;
+}
+
+const transformIdentifierNameResponse = (data: IdentifierNameResponseExpected) => {
+    const formattedData: IdentifierNameResponse  = {
+        numFound: data.numFound,
+        start: data.start,
+        maxScore: data.maxScore,
+        docs: transformIdentifierNameDocs(data.docs)
+    }
+
+    return formattedData;
+}
+
+const transformIdentifierNameDocs = (data: IdentifierNameDocExpected[]) => {
+    const formattedData: IdentifierNameDoc[] = [];
+
+    data.forEach((docExpected) => {
+        const doc = {
+            identifier: convertToStringArray(docExpected.identifier),
+            investigation_name: convertToStringArray(docExpected.investigation_name),
+            instrument_name: convertToStringArray(docExpected.instrument_name),
+            target_name: convertToStringArray(docExpected.target_name),
+            title: convertToStringArray(docExpected.title),
+        }
+
+        formattedData.push(doc);
+    });
+
+    return formattedData;
+}
+
+const convertToStringArray = (param: string | string[]): string[] => Array.isArray(param) ? param : [param];
